72953e5d3ac22a8cb17dbd972f9e92218ccb61f5
gotoAbraxas <lty7085@gmail.com>
2023-10-23 10:35:40 +0900

    feat: 로그인 서비스 기능 제작 #3
    현재는 모든 회원정보를 내놓고 있음.
    이것을 jwt 토큰으로 필요한 정보만 내주어야함.
    이후 티켓 생성하여 작업 예정

--- src/main/java/com/playdata/member/service/MemberService.java
+++ src/main/java/com/playdata/member/service/MemberService.java
@@ -1,5 +1,6 @@
-package com.playdata.service;
+package com.playdata.member.service;
 
+import com.playdata.domain.member.Request.LoginRequest;
 import com.playdata.domain.member.Request.SignupRequest;
 import com.playdata.domain.member.entity.Member;
 import com.playdata.domain.member.repository.MemberRepository;
@@ -20,6 +21,22 @@ public class MemberService {
         }
 
         memberRepository.save(signupRequest.toEntity());
+    }
+
+    public Member login(LoginRequest loginRequest){
+        Member member = findByEmail(loginRequest.getEmail());
+
+        if(member != null){
+            throw new RuntimeException("아이디 혹은 비밀번호가 틀렸습니다.");
+        }
+        if(member.getPassword() != loginRequest.getPassword()){
+            throw new RuntimeException("아이디 혹은 비밀번호가 틀렸습니다.");
+        }
+
+        return member;
+        //TODO 이후 추가적으로 고도화 예정. 비밀번호는 줄 필요 없음.
+        // jwt 토큰으로 발행하여 줄 예정.
+        // 에러 처리도 필요함. 스프링 시큐리티도 적용예정.
     }
 
     public Member findByEmail(String email){

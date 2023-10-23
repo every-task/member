8202eb5571bc3ee7d293fb7394abee098464d698
gotoAbraxas <lty7085@gmail.com>
2023-10-23 14:20:15 +0900

    feat: 비밀번호 암호화 추가 및 jwt 토큰 발행 #7
    리턴값도 member에서 LoginResponse 로 변경

--- src/main/java/com/playdata/member/service/MemberService.java
+++ src/main/java/com/playdata/member/service/MemberService.java
@@ -1,10 +1,13 @@
 package com.playdata.member.service;
 
+import com.playdata.config.JwtService;
 import com.playdata.domain.member.Request.LoginRequest;
 import com.playdata.domain.member.Request.SignupRequest;
 import com.playdata.domain.member.entity.Member;
 import com.playdata.domain.member.repository.MemberRepository;
+import com.playdata.domain.member.response.LoginResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +15,9 @@ import org.springframework.stereotype.Service;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
     public void signup(SignupRequest signupRequest){
         Member byEmail = findByEmail(signupRequest.getEmail());
 
@@ -20,20 +26,30 @@ public class MemberService {
             // 이후에 추가적으로 고도화 예정.
         }
 
-        memberRepository.save(signupRequest.toEntity());
+        Member member = Member.builder()
+                .email(signupRequest.getEmail())
+                .password(passwordEncoder.encode(signupRequest.getPassword()))
+                .profileImageUrl(signupRequest.getProfileImageUrl())
+                .nickname(signupRequest.getNickname())
+                .build();
+
+        memberRepository.save(member);
     }
 
-    public Member login(LoginRequest loginRequest){
+    public LoginResponse login(LoginRequest loginRequest){
         Member member = findByEmail(loginRequest.getEmail());
 
-        if(member != null){
+        if(member == null){
             throw new RuntimeException("아이디 혹은 비밀번호가 틀렸습니다.");
         }
-        if(member.getPassword() != loginRequest.getPassword()){
+        if(!passwordEncoder.matches(loginRequest.getPassword(), member.getPassword())){
             throw new RuntimeException("아이디 혹은 비밀번호가 틀렸습니다.");
         }
 
-        return member;
+        String token = jwtService.makeToken(member);
+        return new LoginResponse(token);
+
+
         //TODO 이후 추가적으로 고도화 예정. 비밀번호는 줄 필요 없음.
         // jwt 토큰으로 발행하여 줄 예정.
         // 에러 처리도 필요함. 스프링 시큐리티도 적용예정.

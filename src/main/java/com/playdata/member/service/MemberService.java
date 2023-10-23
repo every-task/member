34e3f142ff42c53c72efcee58d8d99774731da95
gotoAbraxas <lty7085@gmail.com>
2023-10-23 20:10:00 +0900

    feat: 수정관련 서비스 추가 #9
    이후 에러 처리 및 예외처리 필요

--- src/main/java/com/playdata/member/service/MemberService.java
+++ src/main/java/com/playdata/member/service/MemberService.java
@@ -1,6 +1,9 @@
 package com.playdata.member.service;
 
 import com.playdata.config.JwtService;
+import com.playdata.config.TokenInfo;
+import com.playdata.domain.member.Request.EditInfoRequest;
+import com.playdata.domain.member.Request.EditPassRequest;
 import com.playdata.domain.member.Request.LoginRequest;
 import com.playdata.domain.member.Request.SignupRequest;
 import com.playdata.domain.member.entity.Member;
@@ -9,6 +12,7 @@ import com.playdata.domain.member.response.LoginResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -53,6 +57,30 @@ public class MemberService {
         //TODO 이후 추가적으로 고도화 예정. 비밀번호는 줄 필요 없음.
         // jwt 토큰으로 발행하여 줄 예정.
         // 에러 처리도 필요함. 스프링 시큐리티도 적용예정.
+    }
+
+    @Transactional
+    public void editInfo(TokenInfo tokenInfo, EditInfoRequest editInfoRequest){
+        Member member = memberRepository.findByEmail(tokenInfo.getEmail());
+
+        member.edit(editInfoRequest);
+
+        memberRepository.save(member);
+
+    }
+
+    @Transactional
+    public void editPass(TokenInfo tokenInfo, EditPassRequest editPassRequest){
+        Member member = memberRepository.findByEmail(tokenInfo.getEmail());
+
+        if(!passwordEncoder.matches(editPassRequest.getPassword(), member.getPassword())){
+            throw new RuntimeException("비밀번호가 다릅니다.");
+            // 이후 수정 필요
+        }
+
+        member.editPass(passwordEncoder.encode(editPassRequest.getNewPassword()));
+        memberRepository.save(member);
+
     }
 
     public Member findByEmail(String email){

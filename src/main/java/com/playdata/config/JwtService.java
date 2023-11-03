431e6eb8a60299394b8bc2005f83ca0e78ab5343
gotoAbraxas <lty7085@gmail.com>
2023-11-03 15:42:09 +0900

    feat: 역할 세분화 및 쿠키 발급 추가  #20
    메소드를 잘게 쪼개어서 재사용성을 늘렸습니다.
    쿠키발급 도메인은 추후 local과 prod를 나누어서 관리 예정입니다.

--- src/main/java/com/playdata/config/JwtService.java
+++ src/main/java/com/playdata/config/JwtService.java
@@ -2,7 +2,10 @@ package com.playdata.config;
 
 
 import com.playdata.domain.member.entity.Member;
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.servlet.http.Cookie;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -17,7 +20,7 @@ public class JwtService {
     // 이 키는 다른곳으로 옮겨야한다.
 
 
-    public String makeToken(Member member){
+    public String makeAccessToken(Member member){
         Map<String, Object> claims = new HashMap<>();
 
         claims.put("id",member.getId().toString());
@@ -37,13 +40,64 @@ public class JwtService {
         return token;
     }
 
-    public TokenInfo parseToken(String token){
-        Claims body = (Claims) Jwts.parserBuilder()
+    public TokenInfo parseAccessToken(String token) {
+        Claims body = isValid(token);
+        return buildAccessToken(body);
+    }
+    public UUID parseRefreshToken(String token) {
+        Claims body = isValid(token);
+        return parseUUID(body);
+    }
+
+
+    public Claims isValid(String token){
+        return  (Claims) Jwts.parserBuilder()
                 .setSigningKey(secretKey.getBytes())
                 .build()
                 .parse(token)
                 .getBody();
+    }
 
+    public String makeRefreshToken(String uuid){
+
+        Map<String, Object> claims = new HashMap<>();
+
+        claims.put("id",uuid);
+
+        String refreshToken = Jwts
+                .builder()
+                .setClaims(claims)
+                .setExpiration(new Date(
+                        System.currentTimeMillis()
+                                + + (1000L * 60 * 60 * 24 * 30) // 3일?
+                ))
+                .signWith(SignatureAlgorithm.HS256,secretKey.getBytes())
+                .compact();
+
+        return refreshToken;
+
+    }
+    public Cookie makeCookie(String refreshToken) {
+        Cookie cookie = new Cookie("refreshToken", refreshToken);
+        cookie.setDomain("104.154.101.83"); // 이건 나중에 다시 세팅할것
+        cookie.setHttpOnly(true);
+        // cookie.setSecure(true); // 이건 나중에 https에서 사용가능함
+        cookie.setPath("/");
+        cookie.setMaxAge(1000 * 60 * 60 * 24);
+        return cookie;
+    }
+
+    public Cookie setRefreshTokenInCookie(String uuid){
+        String refreshToken = makeRefreshToken(uuid);
+        return makeCookie(refreshToken);
+
+    }
+
+    private UUID parseUUID(Claims body){
+        return UUID.fromString(body.get("id", String.class));
+    }
+
+    private TokenInfo buildAccessToken(Claims body){
         return TokenInfo.builder()
                 .id(UUID.fromString(body.get("id", String.class)))
                 .email(body.get("email", String.class))

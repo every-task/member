471fb1a967d76ca747132791ae81d27a9f425d3c
gotoAbraxas <lty7085@gmail.com>
2023-11-03 15:59:02 +0900

    feat : 리프래쉬 토큰 재발행에 대한 에러처리 추가  #20

---
+++ src/main/java/com/playdata/domain/member/exception/IncorrectContactException.java
@@ -0,0 +1,7 @@
+package com.playdata.domain.member.exception;
+
+public class IncorrectContactException extends RuntimeException{
+    public IncorrectContactException(String message) {
+        super(message);
+    }
+}

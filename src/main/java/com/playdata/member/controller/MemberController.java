f5ab045d2c1b74ad426554820a83b711cc9325f2
gotoAbraxas <lty7085@gmail.com>
2023-11-03 16:02:51 +0900

    feat : 쿠키 추가를 위한 서블릿 받아오기  #20

--- src/main/java/com/playdata/member/controller/MemberController.java
+++ src/main/java/com/playdata/member/controller/MemberController.java
@@ -8,6 +8,7 @@ import com.playdata.domain.member.Request.SignupRequest;
 import com.playdata.domain.member.response.InfoResponse;
 import com.playdata.domain.member.response.LoginResponse;
 import com.playdata.member.service.MemberService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -21,33 +22,36 @@ public class MemberController {
     private final MemberService memberService;
     @PostMapping("/signup")
     @ResponseStatus(HttpStatus.CREATED)
-    public void signup(@RequestBody SignupRequest signupRequest){
+    public void signup( @RequestBody SignupRequest signupRequest) {
+
         memberService.signup(signupRequest);
     }
 
     @PostMapping("/login")
     @ResponseStatus(HttpStatus.OK)
-    public LoginResponse login(@RequestBody LoginRequest loginRequest){
-        return memberService.login(loginRequest);
-    // 이후 토큰으로 줄 예정.
+    public LoginResponse login(
+            @RequestBody LoginRequest loginRequest,
+            HttpServletResponse response ) {
+
+        return memberService.login(loginRequest,response);
     }
 
     @GetMapping("me/info")
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public InfoResponse getInfo(@AuthenticationPrincipal TokenInfo tokenInfo){
+    public InfoResponse getInfo(@AuthenticationPrincipal TokenInfo tokenInfo) {
         return memberService.getInfo(tokenInfo);
     }
 
     @PutMapping("/me/info")
     @ResponseStatus(HttpStatus.OK)
     public void editInfo(@AuthenticationPrincipal TokenInfo tokenInfo,
-                         @RequestBody EditInfoRequest editInfoRequest){
+                         @RequestBody EditInfoRequest editInfoRequest) {
          memberService.editInfo(tokenInfo, editInfoRequest);
     }
     @PutMapping("/me/password")
     @ResponseStatus(HttpStatus.OK)
     public void editPass(@AuthenticationPrincipal TokenInfo tokenInfo,
-                         @RequestBody EditPassRequest editPassRequest){
+                         @RequestBody EditPassRequest editPassRequest) {
         memberService.editPass(tokenInfo,editPassRequest);
     }
 }

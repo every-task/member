a3f89b89a93ee232f7b0bde48dd08c0a05e1cc83
gotoAbraxas <lty7085@gmail.com>
2023-11-09 18:08:20 +0900

    feat : 로그인시 유저id도 반환한다.  #30

--- src/main/java/com/playdata/domain/member/response/InfoResponse.java
+++ src/main/java/com/playdata/domain/member/response/InfoResponse.java
@@ -8,14 +8,14 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class InfoResponse {
-
+    private String id;
     private String email;
     private String nickname;
     private String profileImageUrl;
 
 
     public InfoResponse(Member member){
-
+        this.id = member.getId().toString();
         this.email = member.getEmail();
         this.nickname = member.getNickname();
         this.profileImageUrl = member.getProfileImageUrl();

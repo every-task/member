8001b2fc45c34b20051ae064c14152e999f3d662
gotoAbraxas <lty7085@gmail.com>
2023-11-16 14:06:25 +0900

    feat : 이메일 검증 추가 #36
    not blank와 이메일만 검증 예정입니다.
    메소드를 따로 만들어서 검증 서비스를 만들기도 한다던데 그건 잘 모르겠습니다.

--- src/main/java/com/playdata/domain/member/Request/SignupRequest.java
+++ src/main/java/com/playdata/domain/member/Request/SignupRequest.java
@@ -1,6 +1,7 @@
 package com.playdata.domain.member.Request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 public class SignupRequest {
 
     @NotBlank
+    @Pattern(regexp = "[a-zA-z0-9]+@[a-zA-z]+[.]+[a-zA-z.]+")
     private String email;
     @NotBlank
     private String password;
